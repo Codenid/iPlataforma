cfab333f8c464dad40d52eb2b828f4a3ba7c246b
Winston Flores <winstonflores30@hotmail.com>
2018-11-18 18:00:19 -0500

    Formulario de Conectato

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,6 +1,5 @@
 $(function() {
-
-  $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+  $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
     preventSubmit: true,
     submitError: function($form, event, errors) {
       // additional error messages or events
@@ -19,39 +18,27 @@ $(function() {
       }
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
-      $.ajax({
-        url: "././mail/contact_me.php",
-        type: "POST",
-        data: {
-          name: name,
-          phone: phone,
-          email: email,
-          message: message
-        },
-        cache: false,
-        success: function() {
+
+      // Envio de correo con js      
+      Email.send("winstonflores30@gmail.com",
+      email,
+      "Saludos desde web de GitHub",
+      "Prueba realizada desde js",
+      {
+        token: "b3b0afbf-099f-46f8-8972-6393a87881b1",
+        callback: function done(message) { 
           // Success message
           $('#success').html("<div class='alert alert-success'>");
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+            .append("<strong>Su mensaje se ha enviado con éxito.</strong>");
           $('#success > .alert-success')
             .append('</div>');
-          //clear all fields
+          // Clear all fields
           $('#contactForm').trigger("reset");
-        },
-        error: function() {
-          // Fail message
-          $('#success').html("<div class='alert alert-danger'>");
-          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
-          $('#success > .alert-danger').append('</div>');
-          //clear all fields
-          $('#contactForm').trigger("reset");
-        },
-        complete: function() {
+
+          // Complete
           setTimeout(function() {
             $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
           }, 1000);
